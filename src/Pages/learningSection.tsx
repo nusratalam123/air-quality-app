4a36621eecb513346f89cfcd88df346870467c22
nusratalam123 <nusratalam975@gmail.com>
2024-10-05 19:48:29 +0600

    learn the value and give msg according to air condition

---
+++ src/Pages/learningSection.tsx
@@ -0,0 +1,12 @@
+
+import AQIInfoTable from "@/Components/qualitytable";
+
+import React from "react";
+export default function Learnsection() {
+  
+  return (
+    <>
+      <AQIInfoTable></AQIInfoTable>
+    </>
+  );
+}
